1f7838daf4d65fa6dbeae35550ae1fcf74933c37
Anon <anon@tmp.com>
2026-01-21 18:31:55 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Stellar-Academy/',
   plugins: [react()],
+  build: {
+    outDir: 'docs',
+  },
 });
